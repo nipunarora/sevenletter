d37ba01d999d4b667fce7c37460f4f9b2481dd65
nipun2512@gmail.com <nipun2512@gmail.com@87e234ae-d58e-11de-8184-f791cb96e2ed>
2009-11-21 20:50:48 +0000

    Initial Project Import

---
+++ SevenLetterWord/src/seven/g0/DumbPlayer4.java
@@ -0,0 +1,39 @@
+package seven.g0;
+
+import java.util.ArrayList;
+import java.util.Random;
+import seven.ui.Letter;
+import seven.ui.Player;
+import seven.ui.PlayerBids;
+import seven.ui.SecretState;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Satyajeet
+ */
+public class DumbPlayer4 implements Player{
+
+    public void Register() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int Bid(Letter bidLetter, ArrayList<PlayerBids> PlayerBidList, int total_rounds,ArrayList<String> PlayerList, SecretState secretstate) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+//        Random rand = new Random();
+//        int val = rand.nextInt(10);
+//        return val;
+        return 10;
+    }
+
+    public String returnWord() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        return "p4";
+    }
+
+
+
+}
