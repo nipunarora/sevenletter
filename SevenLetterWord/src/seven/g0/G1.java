a42d84c94fc33913f69ec3912d71818ece6fa8b4
imladriss <imladriss@87e234ae-d58e-11de-8184-f791cb96e2ed>
2009-11-22 23:07:28 +0000

    Added G1.java. Created hashset for the word list.

---
+++ SevenLetterWord/src/seven/g0/G1.java
@@ -0,0 +1,64 @@
+package seven.g0;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Random;
+
+import seven.ui.CSVReader;
+import seven.ui.Letter;
+import seven.ui.Player;
+import seven.ui.PlayerBids;
+import seven.ui.SecretState;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Satyajeet
+ */
+public class G1 implements Player{
+
+	HashSet<String> wordlist;
+	
+    public void Register() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int Bid(Letter bidLetter, ArrayList<PlayerBids> PlayerBidList, int total_rounds,ArrayList<String> PlayerList, SecretState secretstate) {
+
+        return 0;
+    }
+
+    public String returnWord() {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        return "";
+    }
+
+    public  void initDict()
+    {
+        try{
+            CSVReader csvreader = new CSVReader(new FileReader("sowpods.txt"));
+            String[] nextLine;
+            csvreader.readNext(); // Waste the first line
+            while((nextLine = csvreader.readNext()) != null)
+            {
+                String word = nextLine[1];
+                wordlist.add(word);
+            }
+
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+            System.out.println("\n Could not load dictionary!");
+        }
+
+    }
+   
+
+}
