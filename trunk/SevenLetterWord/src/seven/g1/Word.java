0a628c4fc3810616bf4c7e54f1d6bb16d377e296
benwarfield <benwarfield@87e234ae-d58e-11de-8184-f791cb96e2ed>
2009-11-26 07:12:55 +0000

    Bug-fix in Word (removed workaround for no-longer-present initial spaces).

--- trunk/SevenLetterWord/src/seven/g1/Word.java
+++ trunk/SevenLetterWord/src/seven/g1/Word.java
@@ -1,36 +1,36 @@
 package seven.g1;
 
 public class Word {
-	
+
 	public String word;
 	public int length;
 	//countkeep implementation: the value is the frequency of the letter in the word, and the index is the letter ex A is 0
 	public int[] countKeep= new int[26];
-	
+
 	public Word(String s){
 		word=s;
 		length=s.length();
 		int index;
-		for(int i=1; i<s.length();i++){
+		for(int i = 0; i<s.length();i++){
 			index= Integer.valueOf(s.charAt(i));
 			index=index-Integer.valueOf('A');
 			countKeep[index]++;
 		}
 	}
 	/**
-	 * returns true if the word w can be formed from the letters contained in the word bag object we have currently 
+	 * returns true if the word w can be formed from the letters contained in the word bag object we have currently
 	 * @return
 	 */
 	public boolean issubsetof(Word w){
-		
+
 		for (int i=0;i<26;i++){
 			if(this.countKeep[i]<w.countKeep[i])
 				return false;
 		}
 		return true;
-		
+
 	}
-	
+
 	public String getWord(){
 		return this.word;
 	}
